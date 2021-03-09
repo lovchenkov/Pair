1f2a21964aa20af620cfc63ea79f43a9bb86fb3a
lovchenkov <al.ovchen1329fm@gmail.com>
2021-03-09 11:10:59 +0300

    2

--- test_pair.cpp
+++ test_pair.cpp
@@ -2,15 +2,44 @@
 
 using namespace std;
 
-template<typename T1, typename T2>
-class Pair {
+template <typename T1, typename T2>
+class Pair{
 public:
+    Pair(){}
     T1 first;
     T2 second;
+
+    Pair(const Pair<T1,T2>& copied){
+        first = copied.first;
+        second = copied.second;
+    }
+
+    Pair <T1,T2> operator=(const Pair<T1,T2>& copied) {
+        first = copied.first;
+        second = copied.second;
+        return (*this);
+    }
 };
 
-template<typename T>
-void mySort(vector<T>& a) {
+template <typename T1, typename T2>
+Pair <T1, T2> Make_pair(T1 f, T2 s){
+    Pair <T1, T2> a;
+    a.first = f;
+    a.second = s;
+    return a;
+}
+
+template <typename T>
+void mySort(vector <T>& a) {
+    for(int i = 0; i < a.size(); i ++){
+        for(int j = 0; j < i; j ++){
+            if(a[i]. first < a[j].first || (a[i]. first == a[j].first && a[i]. second < a[j].second)){
+                T carry = a[i];
+                a[i] = a[j];
+                a[j] = carry;
+            }
+        }
+    }
 }
 
 int main() {
